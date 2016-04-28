7210e5eb7bdbaa9f98957d96ebe07634bf957ee2
drafteee <sergej_-96@mail.ru>
2016-04-28 18:47:47 +0300

    wcf

---
+++ WcfChatPost/ChatClient/Login.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ChatClient
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.btnAcceptLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NickName";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(75, 19);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(100, 20);
+            this.tbLogin.TabIndex = 1;
+            // 
+            // btnAcceptLogin
+            // 
+            this.btnAcceptLogin.Location = new System.Drawing.Point(181, 16);
+            this.btnAcceptLogin.Name = "btnAcceptLogin";
+            this.btnAcceptLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnAcceptLogin.TabIndex = 2;
+            this.btnAcceptLogin.Text = "Ok";
+            this.btnAcceptLogin.UseVisualStyleBackColor = true;
+            this.btnAcceptLogin.Click += new System.EventHandler(this.btnAcceptLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 50);
+            this.Controls.Add(this.btnAcceptLogin);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Button btnAcceptLogin;
+    }
+}
